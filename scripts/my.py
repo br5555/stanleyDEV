d9314e740946ab79b6c5ddabb74ed68b0027dc0e
br5555 <branko.rados@fer.hr>
2017-04-02 12:43:08 +0200

    adding picture downlaod

---
+++ scripts/my.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import urllib
+f = open('white.png','wb')
+f.write(urllib.urlopen('https://cloud.githubusercontent.com/assets/7688443/24586308/714e066a-179d-11e7-8d92-ec2e08247e60.png').read())
+f.close()
+
+
