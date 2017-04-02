ba2d03dd9f9092e9e1850d1d247c57e758142d99
br5555 <branko.rados@fer.hr>
2017-04-02 13:07:27 +0200

    picture location changed

--- scripts/my.py
+++ scripts/my.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import urllib
-f = open('white.png','wb')
+f = open('../maps/white.png','wb')
 f.write(urllib.urlopen('https://cloud.githubusercontent.com/assets/7688443/24586308/714e066a-179d-11e7-8d92-ec2e08247e60.png').read())
 f.close()
 
